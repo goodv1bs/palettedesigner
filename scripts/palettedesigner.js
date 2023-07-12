17ccc7fa2e482a8e1e6f874c38546d8b56b0b15b
goodv1bs <cmurphy21@protonmail.com>
2023-07-12 09:55:10 -0700

    started work on the script for theme button, and created the button itself (to be aligned

--- scripts/palettedesigner.js
+++ scripts/palettedesigner.js
@@ -2,6 +2,23 @@ let defaultPalette = true;
 let defaultColor = '#61ca61';
 const $swatchElement = '<div class="swatch-container"><input class="color-id-readout" type="color" value=""></div>';
 //unique identifier for each swatch
+// stores the state of dark mode
+let darkMode = true;
+$().click(function(){
+  // variables here 
+  const darkPalette = {"--body-bg":"#0e0e12","--bg-selector-color":"#535369","--top-bar-color":"#424254"};
+  const lightPalette = {"--body-bg":"#FAFAF7","--bg-selector-color":"#CDE7FA","--top-bar-color":"#E0E0BA",}
+  if (darkMode === true) {
+    // button properties switch the icon to sun
+    // switch darkMode variable to false
+    // swap css properties 
+  }
+  if (darkMode === false) {
+    // button properties switch the icon to moon
+    // switch darkMode variable to true
+    // swap css properties 
+  }
+});
 let swatchId = 0;
 function createSwatch() { // Create new swatch by composing the elements required
   // compose the element
