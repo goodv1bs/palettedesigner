8612dc5939ef8174158274e844e1956d1190105a
goodv1bs <cmurphy21@protonmail.com>
2023-07-14 12:50:04 -0700

    intermediate commit for mode switcher script

--- scripts/palettedesigner.js
+++ scripts/palettedesigner.js
@@ -2,23 +2,6 @@ let defaultPalette = true;
 let defaultColor = '#61ca61';
 const $swatchElement = '<div class="swatch-container"><input class="color-id-readout" type="color" value=""></div>';
 //unique identifier for each swatch
-// stores the state of dark mode
-let darkMode = true;
-$().click(function(){
-  // variables here 
-  const darkPalette = {"--body-bg":"#0e0e12","--bg-selector-color":"#535369","--top-bar-color":"#424254"};
-  const lightPalette = {"--body-bg":"#FAFAF7","--bg-selector-color":"#CDE7FA","--top-bar-color":"#E0E0BA",}
-  if (darkMode === true) {
-    // button properties switch the icon to sun
-    // switch darkMode variable to false
-    // swap css properties 
-  }
-  if (darkMode === false) {
-    // button properties switch the icon to moon
-    // switch darkMode variable to true
-    // swap css properties 
-  }
-});
 let swatchId = 0;
 function createSwatch() { // Create new swatch by composing the elements required
   // compose the element
@@ -50,4 +33,34 @@ $(document).ready(function() {
   $(".add-swatch-container").on("click", function () {
    createSwatch();
   });
+
+  //add switch mode button
+  $("#mode-icon").on("click", function() {
+    modeSwitcher();
+  });
+
 });
+
+// stores the state of dark mode
+let darkMode = true;
+function modeSwitcher(){
+  // stores the new properties of each 
+  const darkPalette = {"--body-bg":"#0e0e12","--bg-selector-color":"#535369","--top-bar-color":"#424254","--plus-icon-color":"#fff"};
+  const lightPalette = {"--body-bg":"#FAFAF7","--bg-selector-color":"#CDE7FA","--top-bar-color":"#E0E0BA","--plus-icon-color":"#000"};
+  if (darkMode === true) {
+    // button properties switch the icon to sun
+    $("#mode-icon i").toggleClass(["fa-moon", "fa-sun"]);
+    // switch darkMode variable to false
+    darkMode = false;
+    // swap css properties 
+    $(":root").css(lightPalette);
+  }
+  if (darkMode === false) {
+    // button properties switch the icon to moon
+    $("#mode-icon i").toggleClass(["fa-moon","fa-sun"]);
+    // switch darkMode variable to true
+    darkMode = true;
+    // swap css properties
+    $(":root").css(darkPalette); 
+  }
+};
