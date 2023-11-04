9d822a86317016e279af0b61ae4d4da3251c6df2
chubbyhorse <doctorslappy233@gmail.com>
2023-11-04 15:36:28 -0700

    moon and sun icon swapping plus light theme tweaks

--- scripts/palettedesigner.js
+++ scripts/palettedesigner.js
@@ -3,7 +3,7 @@ let defaultColor = '#61ca61';
 const $swatchElement = '<div class="swatch-container"><input class="color-id-readout" type="color" value=""></div>';
 //unique identifier for each swatch
 let swatchId = 0;
-let initial = true;
+
 function createSwatch() { // Create new swatch by composing the elements required
   // compose the element
   let newSwatchItem = document.createElement('li');
@@ -16,12 +16,16 @@ function createSwatch() { // Create new swatch by composing the elements require
   $(".color-palette .color-list").append(newSwatchItem);
   
 }
+
 function uniqueIdGenerator() {
   swatchId++;
   return swatchId.toString();
 }
 
-
+function modeSwitcher() {
+  $("body").toggleClass("dark-theme light-theme");
+  $(".mode-icon i").toggleClass("fa-moon fa-sun");
+}
 
 $(document).ready(function() {
   // init new palette
@@ -42,31 +46,3 @@ $(document).ready(function() {
   $("body").toggleClass("dark-theme");
 });
 
-function modeSwitcher() {
-  $("body").toggleClass("dark-theme light-theme");
-}
-
-
-/*// stores the state of dark mode
-let darkMode = true;
-function modeSwitcher(){
-  // stores the new properties of each 
-  const darkPalette = {"--body-bg":"#0e0e12","--bg-selector-color":"#535369","--top-bar-color":"#424254","--plus-icon-color":"#fff"};
-  const lightPalette = {"--body-bg":"#FAFAF7","--bg-selector-color":"#CDE7FA","--top-bar-color":"#E0E0BA","--plus-icon-color":"#000"};
-  if (darkMode === true) {
-    // button properties switch the icon to sun
-    $("#mode-icon i").toggleClass(["fa-moon", "fa-sun"]);
-    // switch darkMode variable to false
-    darkMode = false;
-    // swap css properties 
-    $(":root").css(lightPalette);
-  }
-  if (darkMode === false) {
-    // button properties switch the icon to moon
-    $("#mode-icon i").toggleClass(["fa-moon","fa-sun"]);
-    // switch darkMode variable to true
-    darkMode = true;
-    // swap css properties
-    $(":root").css(darkPalette); 
-  }
-};*/
