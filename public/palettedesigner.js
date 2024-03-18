6b7349079a4750e5587ee5bd40ad77ff67fbe936
chubbyhorse <doctorslappy233@gmail.com>
2024-03-17 22:24:43 -0700

    restructured mock data for now, reworked swatch factory, and added swatch loading

--- public/palettedesigner.js
+++ public/palettedesigner.js
@@ -5,7 +5,7 @@ let swatches = [];
 const $swatchElement = '<div class="swatch-container"><div class="color-picker-wrapper"><label>test</label><input class="color-id-readout" type="color" value="#61ca61"></div></div>';
 //unique identifier for each swatch
 let swatchId = 0;
-
+/*
 function createSwatch() { // Create new swatch by composing the elements required
   // compose the element
   let newSwatchItem = document.createElement('li');
@@ -27,7 +27,7 @@ function createSwatch() { // Create new swatch by composing the elements require
   $('.color-palette .color-list').append(newSwatchItem);
   swatches.push(newestSwatch);
 }
-
+*/
 function uniqueIdGenerator(amountOfSwatches) {
   swatchId = amountOfSwatches.length;
   swatchId++;
@@ -39,15 +39,53 @@ function modeSwitcher() {
   $(".mode-icon i").toggleClass("fa-moon fa-sun");
 }
 
+// Create new swatch by composing the elements required
+function createSwatch(colorToInject) { 
+  let usedColor;
+  if (colorToInject == null) {
+    usedColor = defaultColor;
+  }
+  else {
+    usedColor = colorToInject;
+  }
+  // compose the element
+  let newSwatchItem = document.createElement('li');
+  newSwatchItem.innerHTML = $swatchElement;
+  // set the data attribute
+  newSwatchItem.setAttribute('data-swatchid', uniqueIdGenerator(swatches));
+  // get all the elements we need to manipulate
+  let newestSwatch = $(newSwatchItem).find('.swatch-container');
+  let colorLabel = $(newSwatchItem).find('label');
+  let colorInput = $(newSwatchItem).find('input');
+  // set the default color of the new swatch
+  colorInput.attr('value', usedColor);
+  newestSwatch.css('backgroundColor', usedColor);
+  colorLabel.html(usedColor);
+  // bind the backgrund color of the swatch and the label readout to the value of the color picker
+  colorInput.on("change", () => {
+    newestSwatch.css('backgroundColor', colorInput.val());
+    colorLabel.html(colorInput.val());
+  });
+  // and finally send it to the color list
+  $('.color-palette .color-list').append(newSwatchItem);
+  swatches.push(newestSwatch);
+}
+
 $(document).ready(() => {
   // init new palette
   if(defaultPalette === true) {
     createSwatch();
   }
+  // load it from the whatever
   else {
     fetch('./mockdata/palette.json')
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((data) => {
+        for(color of data.colors) {
+          createSwatch(color);
+        }
+       });
+      //then((data) => console.log(data.colors.swatch1));
   } 
 
   //add swatch button
